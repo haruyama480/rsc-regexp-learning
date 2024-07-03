1ed59cc61f1d95165a198a24106a2b11286a8a4c
haruyama480 <haruyama480@gmail.com>
2024-07-04 02:47:52 +0900

    nfa: impl compiling

--- nfa.go
+++ nfa.go
@@ -1,5 +1,7 @@
 package rscregexplearning
 
+import "fmt"
+
 type State struct {
 	C        int
 	Out      *State
@@ -7,14 +9,82 @@ type State struct {
 	Lastlist int
 }
 
-type Frag struct {
-	Start *State
-	Out   Ptrlist
+func NewMatchState() *State {
+	return &State{C: 257}
 }
 
-type Ptrlist []*State
+func NewSplitState(out, out1 *State) *State {
+	return &State{C: 256, Out: out, Out1: out1}
+}
 
-func list1(outp []*State) Ptrlist           { return nil }
-func append(l1 Ptrlist, l2 Ptrlist) Ptrlist { return nil }
+type Frag struct {
+	Start *State
+	Out   *Ptrlist
+}
 
-func Post2nfa(postfix string) *State { return nil }
+type Ptrlist []**State
+
+func list1(outp **State) Ptrlist {
+	return []**State{outp}
+}
+
+func patch(l *Ptrlist, s *State) {
+	for _, p := range *l {
+		*p = s
+	}
+}
+
+func Post2nfa(postfix string) *State {
+	stack := make([]Frag, 0)
+	push := func(s Frag) {
+		stack = append(stack, s)
+	}
+	pop := func() Frag {
+		s := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		return s
+	}
+	for _, c := range postfix {
+		print(c)
+		switch {
+		case c == '.':
+			e2 := pop()
+			e1 := pop()
+			patch(e1.Out, e2.Start)
+			push(Frag{Start: e1.Start, Out: e2.Out})
+		case c == '|':
+			e2 := pop()
+			e1 := pop()
+			s := NewSplitState(e1.Start, e2.Start)
+			out := append(*e1.Out, *e2.Out...)
+			push(Frag{Start: s, Out: &out})
+		case c == '?':
+			e := pop()
+			s := NewSplitState(e.Start, nil)
+			out := append(*e.Out, list1(&s.Out1)...)
+			push(Frag{Start: s, Out: &out})
+		case c == '*':
+			e := pop()
+			s := NewSplitState(e.Start, nil)
+			patch(e.Out, s)
+			out := list1(&s.Out1)
+			push(Frag{Start: s, Out: &out})
+		case c == '+':
+			e := pop()
+			s := NewSplitState(e.Start, nil)
+			patch(e.Out, s)
+			out := list1(&s.Out1)
+			push(Frag{Start: e.Start, Out: &out})
+		case c < 256:
+			s := &State{C: int(c), Out: nil, Out1: nil}
+			out := list1(&s.Out)
+			push(Frag{Start: s, Out: &out})
+		default:
+			panic(fmt.Sprintf("invalid postfix: %d", c))
+		}
+	}
+
+	e := pop()
+	patch(e.Out, NewMatchState())
+	return e.Start
+}
--- nfa_test.go
+++ nfa_test.go
@@ -1,8 +1,9 @@
 package rscregexplearning
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestPost2nfa(t *testing.T) {
@@ -13,7 +14,8 @@ func TestPost2nfa(t *testing.T) {
 		{
 			postfix: "a",
 			want: &State{
-				C: 'a',
+				C:   'a',
+				Out: NewMatchState(),
 			},
 		},
 		{
@@ -21,15 +23,70 @@ func TestPost2nfa(t *testing.T) {
 			want: &State{
 				C: 'a',
 				Out: &State{
-					C: 'b',
+					C:   'b',
+					Out: NewMatchState(),
 				},
 			},
+		},
+		{
+			postfix: "ab|",
+			want: &State{
+				C: 256,
+				Out: &State{
+					C:   'a',
+					Out: NewMatchState(),
+				},
+				Out1: &State{
+					C:   'b',
+					Out: NewMatchState(),
+				},
+			},
+		},
+		{
+			postfix: "a?",
+			want: &State{
+				C: 256,
+				Out: &State{
+					C:   'a',
+					Out: NewMatchState(),
+				},
+				Out1: NewMatchState(),
+			},
+		},
+		{
+			postfix: "a*",
+			want: func() *State {
+				sp := &State{
+					C: 256,
+					Out: &State{
+						C: 'a',
+					},
+					Out1: NewMatchState(),
+				}
+				sp.Out.Out = sp
+				return sp
+			}(),
+		},
+		{
+			postfix: "a+",
+			want: func() *State {
+				a := &State{
+					C: 'a',
+					Out: &State{
+						C:    256,
+						Out1: NewMatchState(),
+					},
+				}
+				a.Out.Out = a
+				return a
+			}(),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.postfix, func(t *testing.T) {
-			if got := Post2nfa(tt.postfix); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Post2nfa() = %v, want %v", got, tt.want)
+			got := Post2nfa(tt.postfix)
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("Post2nfa() mismatch (-got +want):\n%s", diff)
 			}
 		})
 	}
